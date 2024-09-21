5c58ebc5eed17afe6926506fe4ec24a1ee7432d5
Memet <exortek979@gmail.com>
2024-09-21 16:07:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    nodePolyfills({
+      globals: {
+        global: true,
+        Buffer: true,
+      },
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@src': '/src',
+      '@components': '/src/components',
+      '@providers': '/src/providers',
+      '@lib': '/src/lib',
+      '@hooks': '/src/hooks',
+      '@helpers': '/src/helpers',
+    },
+  },
+});
