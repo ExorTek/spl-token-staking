fc6893de75ac85d8dc605f7cc147310e96656e5d
Memet <exortek979@gmail.com>
2024-09-21 16:07:12 +0300

    Create index.js

---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+export { createStakeAnchorProgram } from './anchor';
+export { stakeSplToken, withdrawSplToken, claimStakingRewards, getUserStakingInfo, getMintStakingPoolInfo } from './staking';
+export { createEndpointConfig } from './solana';
