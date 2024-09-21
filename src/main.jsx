d87a31e3db2db5740c31eada6f9cf7599488453d
Memet <exortek979@gmail.com>
2024-09-21 16:07:17 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import '@solana/wallet-adapter-react-ui/styles.css';
-import './global.css';
+import { SolanaProvider } from '@providers';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SolanaProvider>
+      <App />
+    </SolanaProvider>
   </React.StrictMode>
 );
